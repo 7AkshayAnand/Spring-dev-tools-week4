3af627b2d841916fd421a89b8d1b227f78beb4e7
AKshay Mausam <akshayjha560@gmail.com>
2024-10-12 19:32:51 +0530

    add method is implemented to main branch

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("Hello world!");
 
   System.out.println("main method is running");
+  add(3,4);
+}
+public static void add(int x,int y){
+    System.out.println("addition of 3 and 4 is "+(3+4));
 }
 }
