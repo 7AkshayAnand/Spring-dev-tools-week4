b2802c66cabd4b8cc145ff2da22c30fbf751d763
AKshay Mausam <akshayjha560@gmail.com>
2024-10-12 19:38:10 +0530

    along with add multiplicatio is done

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,14 @@ public class Main {
 
   System.out.println("main method is running");
   add(3,4);
+  multiply(3,4);
 }
-public static void add(int x,int y){
-    System.out.println("addition of 3 and 4 is "+(3+4));
+
+    private static void multiply(int i, int i1) {
+        System.out.println("multiplication  of 3 and 4 is "+(3*4));
+    }
+
+    public static void add(int x,int y){
+    System.out.println("addition  of 3 and 4 is "+(3+4));
 }
 }
